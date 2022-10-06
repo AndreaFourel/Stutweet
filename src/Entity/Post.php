2a26136573915dad6fd0613fa8dd044ae8165a4a
Andrea Fourel <andreafourel@hotmail.fr>
2022-10-06 16:01:42 +0200

    add ORM and create db

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,22 +2,31 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
+#[ORM\Entity()]
+#[ORM\Table(name:"post")]
 class Post
 {
-
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
     private int $id;
 
-    #[Assert\Length(min: 3, max: 150, minMessage: "Le titre doit avoir 3 ou plus caractères!", maxMessage: "Le titre ne doit pas dépasser 150 caractères!")]
+    #[ORM\Column(type: 'string', length: 150, nullable: true)]
     private ?string $title = null;
 
-    #[Assert\NotBlank(message: "Le contenu ne doit pas être vide!")]
-    #[Assert\Length(min: 5, max: 320, minMessage: "Le message doit être compris entre 5 et 320 caractères!", maxMessage: "Le message doit être compris entre 5 et 320 caractères")]
+    //#[Assert\NotBlank(message: "Le contenu ne doit pas être vide!")]
+    //#[Assert\Length(min: 5, max: 320, minMessage: 'Le message doit être compris entre 5 et 320 caractères!', maxMessage: "Le message doit être compris entre 5 et 320 caractères")]
+    #[ORM\Column(type: "text", length: 320)]
     private string $content;
 
-    //voir contraintes de validation ds PostType -> à privilegier
+    //voir contraintes de validation ds PostType-> a privilgier(https://symfony.com/doc/current/best_practices.html#define-validation-constraints-on-the-underlying-object)
+    #[ORM\Column(type: 'text')]
     private ?string $image = null;
+
+    //#[ORM\Column(type: "User")]
     private $user;
 
     public function getId()
@@ -68,16 +77,16 @@ class Post
      return $this;
     }
 
-    public function getUser()
-    {
-     return $this->user;
-    }
-
-
-    public function setUser($user): self
-    {
-     $this->user = $user;
-
-     return $this;
-    }
+//    public function getUser()
+//    {
+//     return $this->user;
+//    }
+//
+//
+//    public function setUser($user): self
+//    {
+//     $this->user = $user;
+//
+//     return $this;
+//    }
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -8,14 +8,29 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Component\Validator\Constraints\Url;
 
 class PostType extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
-    $builder->add("title", TextType::class, ["label" => "Titre", "required" => false])
-    ->add("content", TextareaType::class, ["label" => "Content", "required" => true])
+    $builder->add("title", TextType::class, [
+        "label" => "Titre",
+        "required" => false,
+        "constraints" => [new Length(['min'=>3, 'max'=>150, 'minMessage'=>"Le titre doit avoir au moins {{ limit }} caractères", 'maxMessage'=>'Le titre doit avoir max {{ limit }} caractères!'])]
+    ]
+  )
+    ->add("content", TextareaType::class, [
+        "label" => "Content",
+        "required" => true,
+        "constraints" => [
+            new NotBlank(['message' => 'Le contenu ne doit pas être vide']),
+            new Length(['min' => 3, 'max' => 320, 'minMessage' => 'Le message doit être compris entre 5 et 320 caractères!', 'maxMessage' => 'Le message doit être compris entre 5 et 320 caractères!']),
+        ]
+    ])
     ->add("image", UrlType::class, [
         "label" => "URL de l'image",
         "required" => true,
